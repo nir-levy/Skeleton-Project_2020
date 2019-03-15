75f438258bb4f205dec4aaffe8de8678ec75195c
Alex Pshul <alex@pshul.com>
2019-03-15 12:11:50 +0200

    Added advanced code sample

---
+++ 01 Mobile App/Advanced/CounterApp/CounterApp/CounterApp/CounterModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CounterApp
+{
+    public class CounterModel : INotifyPropertyChanged
+    {
+        public int Id { get; }
+
+        private int _counter;
+        public int Counter
+        {
+            get => _counter;
+            set => SetProperty(ref _counter, value);
+        }
+
+        public ICommand IncrementCounterCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CounterModel(int id)
+        {
+            Id = id;
+            IncrementCounterCommand = new Command(() => Counter++);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ 01 Mobile App/Advanced/CounterApp/CounterApp/CounterApp/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CounterApp
+{
+    public class MainViewModel
+    {
+        public List<CounterModel> Counters { get; }
+
+        public MainViewModel()
+        {
+            Counters = new List<CounterModel>
+            {
+                new CounterModel(1),
+                new CounterModel(2)
+            };
+        }
+    }
+}
